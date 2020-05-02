b43673687c6b8cd17b98e5834b55fd7701386e86
Yiyang Zeng <yz3622@nyu.edu>
2020-05-02 16:27:11 -0400

    Improve tokenization

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,15 +1,23 @@
 from tqdm.auto import tqdm
+from spacy.lang.en import English
+import warnings
+import string 
 
 def tokenize(sentences):
-    from spacy.lang.en import English
+    print("Tokenizing:")
+
     nlp = English()
     # Create a Tokenizer with the default settings for English
     # including punctuation rules and exceptions
     tokenizer = nlp.Defaults.create_tokenizer(nlp)
-    
     results = []
+
     for i in tqdm(range(len(sentences))):
+        # Skip empty sentences
+        if len(sentences[i]) <= 0:
+            continue
+        
         tokens = tokenizer(sentences[i])
-        results.append(sentences[i])      
+        results.append([token.text.lower() for token in tokens])      
         
     return results
